aa217cf7155ce20816021c683558c2da1da77717
zhangxiaoyun <zhangxiaoyun0828@sina.cn>
2018-08-30 00:51:23 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Select></Select>
+  </div>
+</template>
+
+<script>
+import Select from './components/select'
+
+export default {
+  name: 'App',
+  components: {
+    Select
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
