e093cb8650a7731483c30cfb504728e861cec369
zhangxiaoyun <zhangxiaoyun0828@sina.cn>
2018-09-02 21:48:50 +0800

    add demo

---
+++ src/demo/Computed.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      params: {
+        statrTime: '',
+        endTime: ''
+      }
+    }
+  },
+  computed: {
+    time: {
+      get () {
+        return `${this.statrTime}-${this.endTime}`
+      },
+      set (val) {
+        const time = val.split('-') || []
+        this.params.statrTime = time[0]
+        this.params.endTime = time[1]
+      }
+    }
+  }
+}
+</script>
---
+++ src/demo/Template.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="result"></div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+
+  watch: {},
+  created () {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+  .result {
+    width: 20px;
+  }
+</style>
